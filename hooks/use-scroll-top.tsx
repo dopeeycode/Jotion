3cfaa45e94b78ac2a9adf6b6779ad3a1c8cc308d
Italo <italodopeey@gmail.com>
2023-12-15 02:17:42 -0300

    feat: create scroll hook

---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+export function useScrollTop(threshold = 10) {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > threshold) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [threshold])
+
+  return scrolled
+}
