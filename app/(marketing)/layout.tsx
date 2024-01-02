9fd69a911af145048b6cd2002d061751ef24c396
Italo <italodopeey@gmail.com>
2024-01-02 14:43:08 -0300

    style: h-screen in main

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,7 +5,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-fit dark:bg-[#121214]">
       <NavBar />
-      <main className="h-full pt-40">{children}</main>
+      <main className="h-screen pt-40">{children}</main>
     </div>
   )
 }
