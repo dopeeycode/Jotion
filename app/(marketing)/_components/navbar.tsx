6e0824666794f8f30eba4745c1bd6a94f3df4e37
Italo <italodopeey@gmail.com>
2024-01-02 14:34:04 -0300

    feat: verify if user is authenticated, case is not authenticated to show him some options of login

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 'use client'
 
 import { ModeToggle } from '@/components/mode-toggle'
+import Spinner from '@/components/spinner'
+import { Button } from '@/components/ui/button'
 import { useScrollTop } from '@/hooks/use-scroll-top'
 import { cn } from '@/lib/utils'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
+import Link from 'next/link'
 import Logo from './logo'
 
 const NavBar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop()
 
   return (
@@ -17,7 +23,31 @@ const NavBar = () => {
     >
       <Logo />
 
-      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-">
+      <div
+        className="md:ml-auto md:justify-end justify-between w-full flex items-center 
+      gap-x-2"
+      >
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="outline" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
