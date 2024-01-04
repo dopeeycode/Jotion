08d8c06bd9012aaa8ec87759bcb7034e77fa911a
Italo <italodopeey@gmail.com>
2024-01-04 02:15:18 -0300

    feat: first build of Document component

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Spinner from '@/components/spinner'
+import { useConvexAuth } from 'convex/react'
+import { redirect } from 'next/navigation'
+import Navigation from './_components/navigation'
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    )
+  }
+
+  if (!isAuthenticated) {
+    return redirect('/')
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#121214]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  )
+}
+
+export default MainLayout
