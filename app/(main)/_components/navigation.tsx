acf16f13a4d83c9bc3054897d5a3eb1aa68107f7
Italo <italodopeey@gmail.com>
2024-01-04 15:03:23 -0300

    feat: ensuring the general functionalities of navbar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,7 +3,7 @@
 import { cn } from '@/lib/utils'
 import { ChevronsLeft, MenuIcon } from 'lucide-react'
 import { usePathname } from 'next/navigation'
-import { ElementRef, useRef, useState } from 'react'
+import { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 
 const Navigation = () => {
@@ -15,6 +15,20 @@ const Navigation = () => {
   const navbarRef = useRef<ElementRef<'div'>>(null)
   const [isResetting, setIsResetting] = useState(false)
   const [isCollapsed, setIsCollapsed] = useState(isMobile)
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    } else {
+      resetWidth()
+    }
+  }, [isMobile])
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    }
+  }, [pathname, isMobile])
 
   function handleMouseDown(
     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
@@ -47,6 +61,33 @@ const Navigation = () => {
     document.removeEventListener('mouseup', handleMouseUp)
   }
 
+  function resetWidth() {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false)
+      setIsResetting(true)
+
+      sidebarRef.current.style.width = isMobile ? '100%' : '240px'
+      navbarRef.current.style.setProperty(
+        'width',
+        isMobile ? '0' : 'calc(100% - 240px)',
+      )
+      navbarRef.current.style.setProperty('left', isMobile ? '100%' : '240px')
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+
+  function collapse() {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true)
+      setIsResetting(true)
+
+      sidebarRef.current.style.width = '0'
+      navbarRef.current.style.setProperty('width', '100%')
+      navbarRef.current.style.setProperty('left', '0')
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+
   return (
     <>
       <aside
@@ -58,6 +99,7 @@ const Navigation = () => {
         )}
       >
         <div
+          onClick={collapse}
           role="button"
           className={cn(
             'h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition',
@@ -75,6 +117,7 @@ const Navigation = () => {
 
         <div
           onMouseDown={handleMouseDown}
+          onClick={resetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transition 
         cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
         />
@@ -90,7 +133,11 @@ const Navigation = () => {
       >
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && (
-            <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+            />
           )}
         </nav>
       </div>
