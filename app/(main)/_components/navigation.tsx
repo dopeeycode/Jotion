1353a8d1463b30f4988797199d78b12be3d84008
Italo <italodopeey@gmail.com>
2024-01-04 15:56:04 -0300

    feat: create user Item Dropdown

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
+import UserItem from './user-item'
 
 const Navigation = () => {
   const pathname = usePathname()
@@ -109,7 +110,7 @@ const Navigation = () => {
           <ChevronsLeft className="w-6 h-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
